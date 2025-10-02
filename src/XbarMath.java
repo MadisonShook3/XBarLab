22034339a76d24bfcbc8a655cf1835dad82d8b06
Madison Shook <ms6377@ship.engr.edu>
2025-10-02 09:06:20 -0400

    workin on stuff

--- src/XbarMath.java
+++ src/XbarMath.java
@@ -7,7 +7,7 @@ public class XbarMath {
 
   //start with ANOVA to see if the iterations are in control
   public boolean anovaTest(Collection<double[]> categoryData, double alpha) {
-    
+
     return false;
   }
 }
